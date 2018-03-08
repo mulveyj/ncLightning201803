4e3e0dd498b6ca1553880daf0e3e7fb8ae3ff12d
mulveyj <mulveyj@users.noreply.github.com>
2018-03-08 20:06:09 +0000

    Initial commit without tests

---
+++ changeCalculator.js
@@ -0,0 +1,19 @@
+// Please do not change the name of this function
+var changeCalculator = function (pence) {
+  // your code goes here
+  if (typeof pence === 'number' && pence % 1 === 0 && pence >= 0) {
+    let chObj = {};
+  const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+  let left = coins.reduce(function (acc, coin) {
+    let numCoin = Math.floor(acc / coin);
+    if (numCoin > 0) chObj[+coin] = numCoin;
+    return acc % coin;
+  }, pence);
+  return left === 0 ? chObj : 'Error';
+  } 
+};
+
+
+if ( typeof module !== 'undefined' ) {
+  module.exports = {changeCalculator};
+}
